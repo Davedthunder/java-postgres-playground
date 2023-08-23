9a48bf915818798712e30d4b20e58bc742f1c119
Davi  Size <Davedknow@gmail.com>
2023-08-23 20:32:35 +0000

    adicionando testes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,30 +1,82 @@
 package com.example;
 
 
-public class App { 
+public class App {
+
+public static void main(String[] args) {
     
-    public static void main(String[] args) {
-         System.out.println();
-         System.out.println();
-         System.out.println("minha aplicação java");
-         System.out.println();
-         // tipo nomedavarivel;
-        
-          byte idade = 42;
-          
-          double peso = 80.5;
-        
-         System.out.println("Idade é " + idade + " Peso é " + peso );
-         System.out.println();
-        
-         char sexo = 'M';
-         System.out.println("Sexo " + sexo);
-       
-         boolean necessidadeEspeciais = false;
-         System.out.println("tem necessidades especiais " + necessidadeEspeciais );
-       
-       
-       
-        }
-    
+System.out.println();
+System.out.println();
+System.out.println("minha aplicação java");
+
+byte  idade = 22;
+double peso = 70;
+
+System.out.println("idade é " + idade + " peso é " + peso);
+
+char sexo = 'M';
+System.out.println("Sexo " + sexo );
+
+boolean necessidadesEspeciais = false ;
+
+System.out.println("Necessidades especiais " + necessidadesEspeciais);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+ 
