56962be9ceacf86e4d337bd365a4a1ec3639708d
Davi  Size <Davedknow@gmail.com>
2023-08-28 20:39:52 +0000

    mudanças

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -10,48 +10,28 @@ public static void main(String[] args) {
     System.out.println(x);
     x = 20;
     
-    double vetorDouble1[] = {1,2,3};
+    double vetorDouble1[] ={1,2,3};
     double vetorDouble2[] = vetorDouble1;
-    System.out.println(Arrays.toString(vetorDouble2));
+    System.out.println(Arrays.toString(vetorDouble1));  
 
-    System.out.println("valor da posição 0 "+ vetorDouble2[0]);
-    System.out.println("valor da posição 1 "+ vetorDouble2[1]);
-    System.out.println("valor da posição 2 "+ vetorDouble2[2]);
-    System.out.println();
-    vetorDouble2[0] = 1;
-    System.out.println("valor da posição 0"+ vetorDouble2[0]);
-    System.out.println();
-
-    int vetor3[]= new int[4];
+    System.out.println("valor da posição 0 " + vetorDouble2 [0]);
+    System.out.println("valor da posição 1 " + vetorDouble2[1]);
+    System.out.println("valor da posição 2 " + vetorDouble2[2]);
     
-    vetor3[0] = 100;
-    vetor3[1] = 200;
-    vetor3[2] = 300;
-    vetor3[3] = 400;
+    int vetor3[] = new int[4];
+
+    vetor3[0]= 100;
+    vetor3[1]= 200;
+    vetor3[2]= 300;
+    vetor3[3]= 400;
     System.out.println(Arrays.toString(vetor3));
-    System.out.println();
-    
-    double matriz[][]={{10,20,30},{40,50,60}};
-     matriz[0]= new double[]{1,2,3};
 
+    double matriz[][]={{10,20,30},{40,50,60}};
+    
+    matriz[0] = new double[] {10,20,30,40};
+    matriz[1] = new double[] {50,60,70,80,90,100};
     System.out.println(Arrays.toString(matriz[0]));
     System.out.println(Arrays.toString(matriz[1]));
- 
-    System.out.println();
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
 
 
 
